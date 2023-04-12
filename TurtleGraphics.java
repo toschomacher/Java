024cf693ef5bc6631ebc8b221d352f466caff473
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2023-04-12 21:29:44 +0100

    Update TurtleGraphics.java

--- TurtleGraphics.java
+++ TurtleGraphics.java
@@ -4,82 +4,88 @@ import java.awt.Color; //imported by Todor
 //import java.awt.FlowLayout;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import uk.ac.leedsbeckett.oop.LBUGraphics;
 
-class MyHandler implements ActionListener
-{
-	public void actionPerformed(ActionEvent e)
-	{
+class MyHandler implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
 		System.exit(0);
 	}
 }
 
-public class MainClass extends LBUGraphics
-{
+public class MainClass extends LBUGraphics {
 	Letters letter = new Letters();
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		new MainClass(); // create instance of class that extends LBUGraphics (could be separate class
 							// without main), gets out of static context
 	}
-	
-	public void about()	//overriding the about method
+
+	public void about() // overriding the about method
 	{
-		setBackground_Col(Color.MAGENTA);
+		setBackground_Col(Color.DARK_GRAY);
 		clear();
-		//super.about();
+		super.about();
 		myName();
 		System.out.println("My about method called.");
 	}
-	
-	public void myName()
-	{
+
+	public void myName() {
 
 		setTurtleSpeed(1);
 		setPenColour(Color.BLACK);
-		letter.t(300, 300, 3, 10);
-		letter.o(345, 318, 3, 10);
-		letter.d(375, 318, 3, 10);
-		letter.o(405, 318, 3, 10);
-		setxPos(100);
-		setyPos(300);
-		
+		letter.tCap(318, 300, 2, 10);
+		letter.oSml(345, 318, 2, 10);
+		letter.dSml(375, 318, 2, 10);
+		letter.oSml(405, 318, 2, 10);
+		letter.rSml(417, 318, 2, 10);
+		setxPos(105);
+		setyPos(305);
+		setPenColour(Color.WHITE);
+		penDown();
+		circle(50);
+		penUp();
 		System.out.println("Calling myName method.");
 	}
 
-	public MainClass()
-	{	
-		JFrame MainFrame = new JFrame("Turtle Graphics Application"); // create a frame to display the turtle panel on
-		
-		//Button 1
+	public MainClass() {
+		JLabel lbl1 = new JLabel();
+		JFrame mainFrame = new JFrame("Turtle Graphics Application"); // create a frame to display the turtle panel on
+		lbl1.setBounds(805, 18, 275, 90);
+		lbl1.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+		// Button 1
 
-		MainFrame.setResizable(false);
-		MainFrame.setDefaultCloseOperation(3);
-		MainFrame.setLayout(null);
+		mainFrame.setResizable(false);
+		mainFrame.setDefaultCloseOperation(3);
+		mainFrame.setLayout(null);
 		setMaximumSize(null);
-		setPreferredSize(1000, 600);
-		setPanelSize(1000, 600);
-		String txt = "<html>This program is in process of development by Todor Vasilev," +
-					 " a student from LBU CS4D.<br><br>Todor is still learning.</html>";
-		JLabel lbl = new JLabel(txt);
-		lbl.setBounds(810,15,260,95);	//setting the size and position of the label
-		MainFrame.add(lbl);	//adding the label to he window
-		
+		//setPreferredSize(1000, 600);	// not working for now
+		//setPanelSize(1000, 600);		// not working for now
+		String txt = "<html>This program is in process of development by Todor Vasilev, "
+				+ "LBU student - CS4D 2022-2023.<br><br>Student ID - c3643417</html>";
+		JLabel lbl2 = new JLabel(txt);
+		lbl2.setBounds(810, 15, 265, 95); // setting the size and position of the label
+		mainFrame.add(lbl2); // adding the label to the window
+		mainFrame.add(lbl1);
 		JButton btn1 = new JButton("Close");
-		btn1.setBounds(900,340, 100, 30);
-		MainFrame.add(btn1);
+
+		btn1.setBounds(810, 340, 80, 30);
+
+		mainFrame.add(btn1);
+
 		MyHandler hnd = new MyHandler();
 		btn1.addActionListener(hnd);
-		//MainFrame.setLayout(new FlowLayout(2)); // not strictly necessary
-		MainFrame.add(this); // "this" is this object that extends turtle graphics so we are adding a turtle
-								// graphics panel to the frame
-		//MainFrame.setSize(1100,400); // set the frame to a size we can see
-		MainFrame.setBounds(240, 200, 1100, 635);	//1080p 1k resolution
-		//MainFrame.setBounds(390, 270, 1100, 435);	//1440p 2k resolution
-		//MainFrame.setBounds(550, 350, 1100, 435);	//2160p 4k resolution
-		MainFrame.setVisible(true); // now display it
 
-		about(); //call the LBUGraphics about method to display version information.
+		// mainFrame.setLayout(new FlowLayout(2)); // not strictly necessary
+		mainFrame.add(this); // "this" is this object that extends turtle graphics so we are adding a turtle
+								// graphics panel to the frame
+		// mainFrame.setSize(1100,400); // set the frame to a size we can see
+		mainFrame.setBounds(240, 200, 1100, 440); // 1080p 1k resolution
+		// mainFrame.setBounds(390, 270, 1100, 435); //1440p 2k resolution
+		// mainFrame.setBounds(550, 350, 1100, 435); //2160p 4k resolution
+		mainFrame.setVisible(true); // now display it
+
+		about(); // call the LBUGraphics about method to display version information.
 	}
 
 	public void processCommand(String command) // this method must be provided because LBUGraphics will call it when
@@ -96,53 +102,41 @@ public class MainClass extends LBUGraphics
 		int arLength = myArray.length;
 
 		// Setting up variables
-		if (myArray.length > 1)
-		{
+		if (myArray.length > 1) {
 			par = myArray[1];
-			try
-			{
+			try {
 				values = Integer.parseInt(myArray[1]);
 				// true code
-			} catch (NumberFormatException e)
-			{
+			} catch (NumberFormatException e) {
 				// false code
 				values = 0;
 			}
-		} else
-		{
+		} else {
 			par = null;
 			values = 0;
 		}
 
 		// FORWARD
-		if (cmd.equals("forward") && arLength == 1)
-		{
+		if (cmd.equals("forward") && arLength == 1) {
 			forward(20); // Default forward value
-		} else if (cmd.equals("forward") && arLength > 1)
-		{
+		} else if (cmd.equals("forward") && arLength > 1) {
 			forward(values);
 		}
 
 		// Turn LEFT
-		if (cmd.equals("left"))
-		{
-			if (arLength == 1)
-			{
+		if (cmd.equals("left")) {
+			if (arLength == 1) {
 				turnLeft();
-			} else
-			{
+			} else {
 				turnLeft(values);
 			}
 		}
 
 		// Turn RIGHT
-		if (cmd.equals("right"))
-		{
-			if (myArray.length == 1)
-			{
+		if (cmd.equals("right")) {
+			if (myArray.length == 1) {
 				turnRight();
-			} else
-			{
+			} else {
 				par = myArray[1]; // TO DO integer validation
 				values = Integer.parseInt(par);
 				turnRight(values);
@@ -154,8 +148,7 @@ public class MainClass extends LBUGraphics
 			clear();
 
 		// Pen commands for UP DOWN and COLOUR
-		if (cmd.equals("pen"))
-		{
+		if (cmd.equals("pen")) {
 			par = myArray[1];
 			if (par.equals("red"))
 				setPenColour(Color.RED);
@@ -177,118 +170,137 @@ public class MainClass extends LBUGraphics
 
 		// Background colour
 		// still not working
-		if (cmd.equals("background"))
-		{
+		if (cmd.equals("background")) {
 			par = myArray[1];
-			if (par.equals("red"))
-			{
+			if (par.equals("red")) {
 				setBackground_Col(Color.RED);
 				clear();
 			}
-			if (par.equals("green"))
-			{
+			if (par.equals("green")) {
 				setBackground_Col(Color.GREEN);
 				clear();
 			}
-			if (par.equals("blue"))
-			{
+			if (par.equals("blue")) {
 				setBackground_Col(Color.BLUE);
 				clear();
 			}
-			if (par.equals("black"))
-			{
+			if (par.equals("black")) {
 				setBackground_Col(Color.BLACK);
 				clear();
 			}
-			if (par.equals("white"))
-			{
+			if (par.equals("white")) {
 				setBackground_Col(Color.WHITE);
 				clear();
 			}
-			if (par.equals("yellow"))
-			{
+			if (par.equals("yellow")) {
 				setBackground_Col(Color.YELLOW);
 				clear();
 			}
 		}
 
 		// RESET
-		if (cmd.equals("reset"))
-		{
+		if (cmd.equals("reset")) {
 			reset();
+		}
+
+		// ABOUT
+		if (cmd.equals("about")) {
+			clear();
+			reset();
+			about();
 		}
 
 	}
 
-	class Letters
-	{
-		public void t(int x, int y, int stroke, int size)
-		{
+	class Letters {
+		public void tCap(int x, int y, int stroke, int size) {
 			setxPos(x);
 			setyPos(y);
 			setStroke(stroke);
 			penDown();
-			forward(5*size);
+			forward(5 * size);
 			turnLeft(180);
-			forward(5*size);
+			forward(5 * size);
 			turnLeft(90);
-			forward(2*size);
+			forward(2 * size);
 			turnLeft(180);
-			forward(4*size);
+			forward(4 * size);
 			penUp();
 			forward(25);
 			turnRight(90);
 		}
-		public void o(int x, int y, int stroke, int size)
-		{
+
+		public void oSml(int x, int y, int stroke, int size) {
 			setxPos(x);
 			setyPos(y);
 			setStroke(stroke);
 			penDown();
 			turnRight(90);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(2*size);
+			forward(2 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(2*size);
+			forward(2 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(135);
 			penUp();
 			forward(200);
 		}
-		public void d(int x, int y, int stroke, int size)
-		{
+
+		public void dSml(int x, int y, int stroke, int size) {
 			setxPos(x);
 			setyPos(y);
 			setStroke(stroke);
 			penDown();
 			turnRight(90);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(2*size);
+			forward(2 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(1*size);
+			forward(1 * size);
 			turnLeft(45);
-			forward(5*size);
+			forward(5 * size);
 			turnLeft(180);
-			forward(6*size);
+			forward(6 * size);
+			turnLeft(180);
+			forward(3 * size);
+			turnLeft(45);
+			forward(1 * size);
+			turnLeft(135);
 			penUp();
 			forward(200);
+		}
+
+		public void rSml(int x, int y, int stroke, int size) {
+			setxPos(x);
+			setyPos(y);
+			setStroke(stroke);
+			penDown();
+			forward(4 * (int) (size * 0.85));
+			turnLeft(180);
+			forward(3 * (int) (size * 0.85));
+			turnRight(45);
+			forward(1 * size);
+			turnRight(45);
+			forward(1 * size);
+			penUp();
+			forward(55);
+			turnRight(45);
 		}
 	}
 }
