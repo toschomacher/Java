be59928abb443a7b384ee1d421f1520251c09c38
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2023-04-30 20:07:32 +0100

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -5,12 +5,12 @@ import javax.swing.JOptionPane;
 
 public class Commands extends GraphicsSystem
 {
+
 	private static final long serialVersionUID = 1L;
 
 	public Commands()
 	{
-		// super();
-		System.out.println("MainClass constructor called");
+		System.out.println("Commands class constructor called");
 		GUIparts myGUI = new GUIparts();
 		myGUI.myGUI(this);
 		penDown();
@@ -30,6 +30,7 @@ public class Commands extends GraphicsSystem
 		int arLength = myArray.length;
 		boolean validCommand = true;
 		boolean doubleCheck = true;
+
 		// Setting up variables
 		if (arLength == 2)
 		{
@@ -113,28 +114,80 @@ public class Commands extends GraphicsSystem
 
 		// COMMANDS LIST
 
-		// FORWARD
-		if (cmd.equals("forward") && arLength == 1)
+		// ABOUT
+		if (cmd.equals("about"))
 		{
-			displayMessage("Missing parameter. Default Forward called");
-			forward(40); // Default forward value
-		} else if (cmd.equals("forward") && arLength == 2 && parameter1 > 0 && parameter1 < 1000)
-		{
-			displayMessage("Forward by " + parStr1 + " pixels");
-			forward(parameter1);
-		} else if (cmd.equals("forward") && arLength == 2 && parameter1 > 999)
-		{
-			displayMessage(message2);
+			clear();
+			reset();
+			about();
 		}
-		else if (cmd.equals("forward") && arLength == 2 && parameter1 == -1)
+		// ALIGN TURTLE
+		else if (cmd.equals("align"))
 		{
-			displayMessage(message1);
-		} else if (cmd.equals("forward") && arLength == 2 && parameter1 == 0)
+			align(getDirection());
+		}
+		// BACKGROUND COLOUR
+		else if (cmd.equals("background"))
 		{
-			displayMessage("Enter parameter higher than 0");
-		} else if (cmd.equals("forward") && arLength > 2)
-		{
-			displayMessage(message1);
+			if (arLength == 1)
+			{
+				displayMessage("Missing parameter");
+				return;
+			} else if (arLength == 2)
+			{
+				parStr1 = myArray[1];
+				if (parStr1.equals("red"))
+				{
+					setBackground_Col(Color.RED);
+					displayMessage("Background set to red");
+					clear();
+				} else if (parStr1.equals("green"))
+				{
+					setBackground_Col(Color.GREEN);
+					displayMessage("Background set to green");
+					clear();
+				} else if (parStr1.equals("blue"))
+				{
+					setBackground_Col(Color.BLUE);
+					displayMessage("Background set to blue");
+					clear();
+				} else if (parStr1.equals("black"))
+				{
+					setBackground_Col(Color.BLACK);
+					displayMessage("Background set to black");
+					clear();
+				} else if (parStr1.equals("white"))
+				{
+					setBackground_Col(Color.WHITE);
+					displayMessage("Background set to white");
+					clear();
+				} else if (parStr1.equals("yellow"))
+				{
+					setBackground_Col(Color.YELLOW);
+					displayMessage("Background set to yellow");
+					clear();
+				} else
+				{
+					displayMessage(message1);
+				}
+			}
+			// BACKGROUND R G B
+			else if (arLength == 4)
+			{
+				if (parameter1 >= 0 && parameter1 <= 255 && parameter2 >= 0 && parameter2 <= 255 && parameter3 >= 0
+						&& parameter3 <= 255)
+				{
+					displayMessage("Custom background colour set");
+					setBackground_Col(myColour(parameter1, parameter2, parameter3));
+					clear();
+				} else
+				{
+					displayMessage("Invalid parameter. Each parameter should be a number between 0 and 255");
+				}
+			} else
+			{
+				displayMessage(message1);
+			}
 		}
 		// BACKWARD
 		else if (cmd.equals("backward") && arLength == 1)
@@ -148,8 +201,7 @@ public class Commands extends GraphicsSystem
 		} else if (cmd.equals("backward") && arLength == 2 && parameter1 > 999)
 		{
 			displayMessage(message2);
-		}
-		else if (cmd.equals("backward") && arLength == 2 && parameter1 == -1)
+		} else if (cmd.equals("backward") && arLength == 2 && parameter1 == -1)
 		{
 			displayMessage(message1);
 		} else if (cmd.equals("backward") && arLength == 2 && parameter1 == 0)
@@ -159,40 +211,60 @@ public class Commands extends GraphicsSystem
 		{
 			displayMessage(message1);
 		}
-		// Turn LEFT
-		else if (cmd.equals("left"))
-		{
-			if (arLength == 1)
-			{
-				displayMessage("Missing parameter. Default Left called");
-				turnLeft();
-			} else
-			{
-				turnLeft(parameter1);
-			}
-		}
-		// Turn RIGHT
-		else if (cmd.equals("right"))
-		{
-			if (arLength == 1)
-			{
-				displayMessage("Missing parameter. Default Right called");
-				turnRight();
-			} else
-			{
-				turnRight(parameter1);
-			}
-		}
 		// CLEAR
 		else if (cmd.equals("clear"))
 		{
-			clear();
-			if (arLength > 1)
+			if (screenUpdate)
 			{
-				displayMessage("");
+				int dialogButton = JOptionPane.showConfirmDialog(null,
+						"Graphics not saved. " + "Are you sure you want to clear the screen without saving?",
+						"Unsaved work!", JOptionPane.YES_NO_CANCEL_OPTION);
+				if (dialogButton == JOptionPane.YES_OPTION)
+				{
+					clear();
+					screenUpdate = false;
+				} else if (dialogButton == JOptionPane.NO_OPTION)
+				{
+					turnLeft();
+				} else if (dialogButton == JOptionPane.CANCEL_OPTION)
+				{
+					turnRight();
+				}
+			} else
+			{
+				clear();
+				screenUpdate = false;
 			}
+
 		}
-		// Pen commands for UP DOWN and COLOUR
+		// FORWARD
+		else if (cmd.equals("forward") && arLength == 1)
+		{
+			displayMessage("Missing parameter. Default Forward called");
+			forward(40); // Default forward value
+		} else if (cmd.equals("forward") && arLength == 2 && parameter1 > 0 && parameter1 < 1000)
+		{
+			displayMessage("Forward by " + parStr1 + " pixels");
+			forward(parameter1);
+		} else if (cmd.equals("forward") && arLength == 2 && parameter1 > 999)
+		{
+			displayMessage(message2);
+		} else if (cmd.equals("forward") && arLength == 2 && parameter1 == -1)
+		{
+			displayMessage(message1);
+		} else if (cmd.equals("forward") && arLength == 2 && parameter1 == 0)
+		{
+			displayMessage("Enter parameter higher than 0");
+		} else if (cmd.equals("forward") && arLength > 2)
+		{
+			displayMessage(message1);
+		}
+		// GET DIRECTION
+		else if (cmd.equals("direction"))
+		{
+			displayMessage("Turtle at " + Integer.toString(getDirection()) + " degrees");
+		}
+		// PEN commands for UP DOWN and COLOUR
 		else if (cmd.equals("pen"))
 		{
 			if (myArray.length == 1)
@@ -258,6 +330,18 @@ public class Commands extends GraphicsSystem
 		{
 			displayMessage("Missing parameter");
 		}
+		// PEN STATE
+		else if (cmd.equals("penstate"))
+		{
+			if (getPenState())
+			{
+				displayMessage("Pen is DOWN");
+
+			} else
+			{
+				displayMessage("Pen is UP");
+			}
+		}
 		// PEN STROKE GETTER
 		else if (cmd.equals("penwidth") && arLength == 1)
 		{
@@ -281,68 +365,16 @@ public class Commands extends GraphicsSystem
 				displayMessage("Pen width parameter shoud be a value betwean 1 and 10");
 			}
 		}
-		// Background colour
-		else if (cmd.equals("background"))
+		// RESET
+		else if (cmd.equals("reset"))
 		{
-			if (arLength == 1)
-			{
-				displayMessage("Missing parameter");
-				return;
-			} else if (arLength == 2)
-			{
-				parStr1 = myArray[1];
-				if (parStr1.equals("red"))
-				{
-					setBackground_Col(Color.RED);
-					displayMessage("Background set to red");
-					clear();
-				} else if (parStr1.equals("green"))
-				{
-					setBackground_Col(Color.GREEN);
-					displayMessage("Background set to green");
-					clear();
-				} else if (parStr1.equals("blue"))
-				{
-					setBackground_Col(Color.BLUE);
-					displayMessage("Background set to blue");
-					clear();
-				} else if (parStr1.equals("black"))
-				{
-					setBackground_Col(Color.BLACK);
-					displayMessage("Background set to black");
-					clear();
-				} else if (parStr1.equals("white"))
-				{
-					setBackground_Col(Color.WHITE);
-					displayMessage("Background set to white");
-					clear();
-				} else if (parStr1.equals("yellow"))
-				{
-					setBackground_Col(Color.YELLOW);
-					displayMessage("Background set to yellow");
-					clear();
-				} else
-				{
-					displayMessage(message1);
-				}
-			}
-			// BACKGROUND R G B
-			else if (arLength == 4)
-			{
-				if (parameter1 >= 0 && parameter1 <= 255 && parameter2 >= 0 && parameter2 <= 255 && parameter3 >= 0
-						&& parameter3 <= 255)
-				{
-					displayMessage("Custom background colour set");
-					setBackground_Col(myColour(parameter1, parameter2, parameter3));
-					clear();
-				} else
-				{
-					displayMessage("Invalid parameter. Each parameter should be a number between 0 and 255");
-				}
-			} else
-			{
-				displayMessage(message1);
-			}
+			reset();
+			displayMessage("Reset successful");
+		}
+		// SAVE LOAD FILE
+		else if (cmd.equals("save") && arLength == 2 && parStr1.equals("commands"))
+		{
+			FileHandling();
 		}
 		// SQUARE
 		else if (cmd.equals("square") && arLength == 1)
@@ -371,35 +403,36 @@ public class Commands extends GraphicsSystem
 			displayMessage("Drawing custom triangle");
 			triangle(parameter1, parameter2, parameter3);
 		}
-		// RESET
-		else if (cmd.equals("reset"))
+		// TURN LEFT
+		else if (cmd.equals("left"))
 		{
-			reset();
-			displayMessage("Reset successful");
+			if (arLength == 1)
+			{
+				displayMessage("Missing parameter. Default Left called");
+				turnLeft();
+			} else
+			{
+				turnLeft(parameter1);
+			}
 		}
-		// ABOUT
-		else if (cmd.equals("about"))
+		// TURN RIGHT
+		else if (cmd.equals("right"))
 		{
-			clear();
-			reset();
-			about();
+			if (arLength == 1)
+			{
+				displayMessage("Missing parameter. Default Right called");
+				turnRight();
+			} else
+			{
+				turnRight(parameter1);
+			}
 		}
-		// ALIGN TURTLE
-		else if (cmd.equals("align"))
-		{
-			align(getDirection());
-		}
-		// GET DIRECTION
-		else if (cmd.equals("direction"))
-		{
-			displayMessage("Turtle at " + Integer.toString(getDirection()) + " degrees");
-		}
-		// X POSITION
+		// X GET POSITION
 		else if (cmd.equals("xpos"))
 		{
 			xPosition();
 		}
-		// SET X POS
+		// X SET POSITION
 		else if (cmd.equals("setx") && arLength == 2 && parameter1 >= 0 && parameter1 <= 800)
 		{
 			setxPos(parameter1);
@@ -408,12 +441,12 @@ public class Commands extends GraphicsSystem
 		{
 			displayMessage("Enter parameter between 0 and 800");
 		}
-		// Y POSITION
+		// Y GET POSITION
 		else if (cmd.equals("ypos"))
 		{
 			yPosition();
 		}
-		// SET Y POS
+		// Y SET POSITION
 		else if (cmd.equals("sety") && arLength == 2 && parameter1 >= 0 && parameter1 <= 400)
 		{
 			setyPos(parameter1);
@@ -421,7 +454,9 @@ public class Commands extends GraphicsSystem
 		} else if (cmd.equals("sety"))
 		{
 			displayMessage("Enter parameter between 0 and 800");
-		} else
+		}
+		// FINAL ELSE validating commands
+		else
 		{
 			validCommand = false;
 		}
